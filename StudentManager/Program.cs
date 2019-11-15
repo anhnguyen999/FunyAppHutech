09fed0b8d5c1e80642625c58b5837efc318fbe8c
Anh <nd.anh@hutech.edu.vn>
2019-11-15 11:14:20 +0700

    Nop bai tap 2 - Nguyen van A, Nguyen Van B

---
+++ StudentManager/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManager
+{
+    class Program
+    {
+        static Student[] studentList;
+
+        static void Main(string[] args)
+        {
+            //nhap tong so sinh vien
+            int numOfStudent;
+            Console.Write("Num of Student = ");
+            try
+            {
+                numOfStudent = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Input Number Of Student! - {0}", ex.Message);
+                Console.ReadKey();
+                return;
+            }
+
+            //Tao dah sach sinh vien
+            InputStudentList(numOfStudent);
+            OutputStudentList();
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Hàm xuất danh sách sinh viên
+        /// </summary>
+        private static void OutputStudentList()
+        {
+            foreach (var student in studentList)
+            {
+                Console.WriteLine("Student ID = {0}", student.StudentID);
+                //tuong tu
+            }
+        }
+
+        private static void InputStudentList(int numOfStudent)
+        {
+            //Tao mang luu danh sach sinh vien
+            studentList = new Student[numOfStudent];
+            Student student;
+            for (int i = 0; i < numOfStudent; i++)
+            {
+                student = new Student();
+                Console.Write("Student ID = ");
+                student.StudentID = int.Parse(Console.ReadLine());
+                //Luu doi tuong sinh vien vao danh sach
+                studentList[i] = student;
+            }
+        }
+    }
+}
---
+++ StudentManager/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManager
+{
+    class Student
+    {
+        //ctrl + k + C: Comment 
+        //ctrl + k + U: UnComment 
+        //ctrl + k + D: Format code 
+        public int StudentID { get; set; }
+        public string FullName { get; set; }
+        public float Mark { get; set; }
+        public string Falculty { get; set; }
+
+        public Student()
+        {
+
+        }
+
+        public Student(int studentID, string fullName, float mark, string faculty)
+        {
+            StudentID = studentID;
+            FullName = fullName;
+            Mark = mark;
+            Falculty = faculty;
+        }
+    }
+}
