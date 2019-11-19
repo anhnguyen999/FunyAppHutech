cc48c567bbf73e9462a1a5855e316d650f781c64
Khanghao <khanghaotitan@gmail.com>
2019-11-19 09:52:32 +0700

    Bai tap so 3

--- StudentManager/Program.cs
+++ StudentManager/Program.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/// <summary>
+/// Nhom Solo: Tran Trong Khang
+/// Bai tap so 3
+/// </summary>
 namespace StudentManager
 {
     class Program
