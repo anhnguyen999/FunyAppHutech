328369d1542b3485ec2cf26aee2ff93918c3e0ba
kim ngân <nganbuinguyen123@gmail.com>
2019-11-22 22:21:53 +0700

    Bai 3_Lab 1 Nhom HBT

---
+++ HoaDonDien/KhachHangNuocNgoai.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoaDonDien
+{
+    class KhachHangNuocNgoai : KhachHang
+    {
+        public String QuocTich { get; set; }
+
+        public override double ThanhTien()
+        {
+            return SoLuongKW * DonGia;
+        }
+        public override void Input()
+        {
+            base.Input();
+            Console.Write("Quoc tich: ");
+            QuocTich = Console.ReadLine();
+        }
+        public override void Output()
+        {
+            base.Output();
+            Console.WriteLine("Quoc tich: " + QuocTich);
+            Console.WriteLine("Thanh tien: " + ThanhTien());
+        }
+    }
+}
