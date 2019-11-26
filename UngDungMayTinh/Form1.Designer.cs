753afd6b34d0f5453308999376e47f1551c994e4
Thức Đặng Đông <waitingky@live.com>
2019-11-26 11:14:18 +0700

    26112019 - nhom ACb nop bai

---
+++ UngDungMayTinh/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace UngDungMayTinh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSoa = new System.Windows.Forms.TextBox();
+            this.txtSob = new System.Windows.Forms.TextBox();
+            this.txtKetqua = new System.Windows.Forms.TextBox();
+            this.btnPhepCong = new System.Windows.Forms.Button();
+            this.btnPhepTru = new System.Windows.Forms.Button();
+            this.btnPhepNhan = new System.Windows.Forms.Button();
+            this.btnPhepChia = new System.Windows.Forms.Button();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(192, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(438, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ỨNG DỤNG MÁY TÍNH";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(145, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(145, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 36);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Số B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(145, 311);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 36);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "KQ";
+            // 
+            // txtSoa
+            // 
+            this.txtSoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSoa.Location = new System.Drawing.Point(291, 136);
+            this.txtSoa.Name = "txtSoa";
+            this.txtSoa.Size = new System.Drawing.Size(315, 41);
+            this.txtSoa.TabIndex = 4;
+            // 
+            // txtSob
+            // 
+            this.txtSob.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSob.Location = new System.Drawing.Point(291, 183);
+            this.txtSob.Name = "txtSob";
+            this.txtSob.Size = new System.Drawing.Size(315, 41);
+            this.txtSob.TabIndex = 5;
+            // 
+            // txtKetqua
+            // 
+            this.txtKetqua.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKetqua.Location = new System.Drawing.Point(291, 311);
+            this.txtKetqua.Name = "txtKetqua";
+            this.txtKetqua.Size = new System.Drawing.Size(315, 41);
+            this.txtKetqua.TabIndex = 6;
+            // 
+            // btnPhepCong
+            // 
+            this.btnPhepCong.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPhepCong.Location = new System.Drawing.Point(222, 242);
+            this.btnPhepCong.Name = "btnPhepCong";
+            this.btnPhepCong.Size = new System.Drawing.Size(75, 47);
+            this.btnPhepCong.TabIndex = 7;
+            this.btnPhepCong.Text = "+";
+            this.btnPhepCong.UseVisualStyleBackColor = true;
+            this.btnPhepCong.Click += new System.EventHandler(this.BtnPhepTinh_Click);
+            // 
+            // btnPhepTru
+            // 
+            this.btnPhepTru.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPhepTru.Location = new System.Drawing.Point(303, 242);
+            this.btnPhepTru.Name = "btnPhepTru";
+            this.btnPhepTru.Size = new System.Drawing.Size(75, 47);
+            this.btnPhepTru.TabIndex = 8;
+            this.btnPhepTru.Text = "-";
+            this.btnPhepTru.UseVisualStyleBackColor = true;
+            this.btnPhepTru.Click += new System.EventHandler(this.BtnPhepTinh_Click);
+            // 
+            // btnPhepNhan
+            // 
+            this.btnPhepNhan.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPhepNhan.Location = new System.Drawing.Point(384, 242);
+            this.btnPhepNhan.Name = "btnPhepNhan";
+            this.btnPhepNhan.Size = new System.Drawing.Size(75, 47);
+            this.btnPhepNhan.TabIndex = 9;
+            this.btnPhepNhan.Text = "*";
+            this.btnPhepNhan.UseVisualStyleBackColor = true;
+            this.btnPhepNhan.Click += new System.EventHandler(this.BtnPhepTinh_Click);
+            // 
+            // btnPhepChia
+            // 
+            this.btnPhepChia.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPhepChia.Location = new System.Drawing.Point(465, 242);
+            this.btnPhepChia.Name = "btnPhepChia";
+            this.btnPhepChia.Size = new System.Drawing.Size(75, 47);
+            this.btnPhepChia.TabIndex = 10;
+            this.btnPhepChia.Text = "/";
+            this.btnPhepChia.UseVisualStyleBackColor = true;
+            this.btnPhepChia.Click += new System.EventHandler(this.BtnPhepTinh_Click);
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPhepChia);
+            this.Controls.Add(this.btnPhepNhan);
+            this.Controls.Add(this.btnPhepTru);
+            this.Controls.Add(this.btnPhepCong);
+            this.Controls.Add(this.txtKetqua);
+            this.Controls.Add(this.txtSob);
+            this.Controls.Add(this.txtSoa);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSoa;
+        private System.Windows.Forms.TextBox txtSob;
+        private System.Windows.Forms.TextBox txtKetqua;
+        private System.Windows.Forms.Button btnPhepCong;
+        private System.Windows.Forms.Button btnPhepTru;
+        private System.Windows.Forms.Button btnPhepNhan;
+        private System.Windows.Forms.Button btnPhepChia;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+    }
+}
+
