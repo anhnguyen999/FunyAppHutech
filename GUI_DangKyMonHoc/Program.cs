9db098f2dfd66e323de467ad7dee291c98235acf
Thức Đặng Đông <waitingky@live.com>
2019-12-03 11:28:23 +0700

    030122019 - nhom ACB

---
+++ GUI_DangKyMonHoc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_DangKyMonHoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- UngDungMayTinh/Form1.cs
+++ UngDungMayTinh/Form1.cs
@@ -12,6 +12,7 @@ namespace UngDungMayTinh
 {
     public partial class Form1 : Form
     {
+        private string messageError;
         public Form1()
         {
             InitializeComponent();
@@ -19,13 +20,16 @@ namespace UngDungMayTinh
 
         private void BtnPhepTinh_Click(object sender, EventArgs e)
         {
+            if (!ValidateInputNumber(out float soA, out float soB))
+            {
+                MessageBox.Show(messageError, "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
             //Chuyển sender về button
             Button btnPhepTinh = (Button)sender;
             //Xac dinh phep tinh duoc chon dua theo tên của sender
             string phepTinh = btnPhepTinh.Name;
             //lấy giá trị nhập
-            float soA = float.Parse(txtSoA.Text);
-            float soB = float.Parse(txtSoB.Text);
             float ketQua = 0;
             //Thuc hien phép tính bằng cấu trúc lựa chọn
             switch (phepTinh)
@@ -46,6 +50,30 @@ namespace UngDungMayTinh
                     break;
             }
             txtKetQua.Text = ketQua.ToString();
+        }
+
+        private bool ValidateInputNumber(out float soA, out float soB)
+        {
+            messageError = "";
+            try
+            {
+                soA = float.Parse(txtSoA.Text);
+            }
+            catch (Exception)
+            {
+                messageError = "Số A không hợp lệ";
+            }
+            if(!float.TryParse(txtSoA.Text, out soA))
+            {
+                messageError = "Số A nhập không hợp lệ";
+            }
+            if(!float.TryParse(txtSoB.Text, out soB))
+            {
+                messageError += "\nSố B nhập không hợp lệ";
+            }
+            if (messageError == "")
+                return true;
+            return false;
         }
 
         private void TxtNumber_KeyPress(object sender, KeyPressEventArgs e)
